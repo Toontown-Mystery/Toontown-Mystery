63552f5dd2588c9ed508720d9c3d89708f355b74
Master Cool Cat <35885621+BardKitten@users.noreply.github.com>
2022-08-13 11:17:46 -0400

    Rebalanced gags, facilities, bosses, and buildings.

--- toontown/battle/BattleCalculatorAI.py
+++ toontown/battle/BattleCalculatorAI.py
@@ -10,9 +10,9 @@ from direct.showbase.PythonUtil import lerp
 
 class BattleCalculatorAI:
     AccuracyBonuses = [
-     0, 20, 40, 60]
+     5, 20, 35, 50]
     DamageBonuses = [
-     0, 100, 150, 200]
+     0, 50, 75, 100]
     AttackExpPerTrack = [
      0, 10, 20, 30, 40, 50, 60]
     NumRoundsLured = AvLureRounds
@@ -838,7 +838,7 @@ class BattleCalculatorAI:
                         if self.notify.getDebug():
                             self.notify.debug('Applying hp bonus to track ' + str(attack[TOON_TRACK_COL]) + ' of ' + str(attack[TOON_HPBONUS_COL]))
                     elif len(attack[TOON_KBBONUS_COL]) > tgtPos:
-                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 1.25
+                        attack[TOON_KBBONUS_COL][tgtPos] = totalDmgs * 0.75
                         if self.notify.getDebug():
                             self.notify.debug('Applying kb bonus to track ' + str(attack[TOON_TRACK_COL]) + ' of ' + str(attack[TOON_KBBONUS_COL][tgtPos]) + ' to target ' + str(tgtPos))
                     else:
